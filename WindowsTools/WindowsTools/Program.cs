a2416f05d41986af0db02c47a8227c7154e89c74
Jagoba Inda <jindaardam1@gmail.com>
2024-02-22 20:44:26 +0100

    Malware project (has "WindowsTools" name) added

---
+++ WindowsTools/WindowsTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsTools
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Malware");
+        }
+    }
+}
