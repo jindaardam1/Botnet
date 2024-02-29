725ea0ca26938d288bc608c3a4affac660d412f6
Jagoba Inda <jindaardam1@gmail.com>
2024-02-29 05:02:31 +0100

    Dashboard progress

--- TailsDashboard/main.js
+++ TailsDashboard/main.js
@@ -1,7 +1,51 @@
 window.addEventListener('DOMContentLoaded', function () {
-
+    addTooltipListeners();
+    addButtonEnviarComandoListener();
 });
 
-function encryptCommandToSha512(command) {
-    return sha512(command);
+
+// Function to add listeners for tooltips
+function addTooltipListeners() {
+    const tooltip = document.getElementById('contra');
+    const tooltipText = document.querySelector('.tooltiptext');
+
+    tooltip.addEventListener('mouseenter', () => {
+
+        tooltipText.style.visibility = 'visible';
+        tooltipText.style.opacity = '1';
+
+    });
+
+    tooltip.addEventListener('mouseleave', () => {
+
+        tooltipText.style.visibility = 'hidden';
+        tooltipText.style.opacity = '0';
+
+    });
+}
+
+
+// Function to add listener to the send command button
+function addButtonEnviarComandoListener() {
+    const enviarComandoButton = document.getElementById("buttonEnviarComando");
+
+    enviarComandoButton.addEventListener('click', () => {
+        sendCommand();
+    });
+}
+
+
+// Function to encrypt text to SHA512, such as commands or passwords
+function encryptToSha512(text) {
+    return sha512(text);
+}
+
+
+// Function to send command to the Command & Control Server
+function sendCommand() {
+    const contraInput = document.getElementById("contra");
+
+    let encriptedContra = encryptToSha512(contraInput.value);
+
+    console.log(encriptedContra);
 }
