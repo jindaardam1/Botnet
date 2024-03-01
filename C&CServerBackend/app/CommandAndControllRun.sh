3e61a17736ad91d0494d619b53646bcb99392dc2
jindaardam1 <jindaardam1@gmail.com>
2024-03-01 12:11:08 +0100

    CommandAndControllRun.sh update

--- C&CServerBackend/app/CommandAndControllRun.sh
+++ C&CServerBackend/app/CommandAndControllRun.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Función para instalar Python si no está instalado
+install_python_if_its_not() {
+    if ! command -v python3 &> /dev/null; then
+        echo "Instalando Python..."
+        sudo apt-get install -y python3
+    else
+        echo "Python está instalado."
+    fi
+}
+
 # Función para instalar un paquete si no existe
 install_package_if_not_exists() {
-    if ! dpkg -l | grep -q "$1"; then
+    if ! pip show "$1" > /dev/null 2>&1; then
         echo "Instalando el paquete $1..."
-        sudo apt-get install -y "$1"
+        sudo pip install "$1"
     else
         echo "El paquete $1 ya está instalado."
     fi
@@ -13,9 +23,10 @@ install_package_if_not_exists() {
 # Función para instalar los requisitos de Python
 install_python_requirements() {
     echo "Instalando los requisitos de Python..."
-    install_package_if_not_exists "python3-pytz"
-    install_package_if_not_exists "python3-hashlib"
-    install_package_if_not_exists "python3-datetime"
+    install_package_if_not_exists "pytz"
+    install_package_if_not_exists "hashlib"
+    install_package_if_not_exists "datetime"
+    install_package_if_not_exists "colorama"
     echo "Los requisitos de Python han sido instalados correctamente."
 }
 
@@ -25,5 +36,11 @@ run_command_and_control_server() {
     python3 ../src/core/main.py
 }
 
+# Establecer la codificación de la terminal a UTF-8
+export LANG=en_US.UTF-8
+
+install_python_if_its_not
+
 install_python_requirements
+
 run_command_and_control_server
