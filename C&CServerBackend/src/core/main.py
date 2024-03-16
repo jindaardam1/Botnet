ca7b7586cc756c240cfac7d29871a2089e874eb7
Jagoba Inda <jindaardam1@gmail.com>
2024-03-16 18:47:30 +0100

    main

--- C&CServerBackend/src/core/main.py
+++ C&CServerBackend/src/core/main.py
@@ -1,2 +1,6 @@
-while True:
-    pass
+from src.model.dao.devices_dao import DevicesDAO
+from src.model.entities.device import Device
+
+dao = DevicesDAO()
+
+dao.create_device(Device('a', 'a', 'a', 'a'))
